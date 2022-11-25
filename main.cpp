7015f1d7d68f439324a0759601a271e75e60f5de
BAgithub2022 <117846641+BAgithub2022@users.noreply.github.com>
2022-11-25 23:15:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+    int n=0;
+   int a=0,b=1,risult=0;
+   std::cin>>n;
+   std::cout << "tutti i numeri della serie di Fibonacci che sono "<<" "<<"<="<<" "<<n<<std::endl;
+   std::cout<<1<<std::endl;
+   for (int i = n; i >=1 ; --i) {
+      risult = a + b;
+      a = b;
+      b = risult;
+      if (risult<=n) {
+        std::cout << risult << std::endl;
+          }
+      }
    return 0;
 }
